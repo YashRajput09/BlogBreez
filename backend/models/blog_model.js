0e8deb1dd618fe4bc70f12fa0ad6a3936ccb70b3
Yash Rajput <rajputyash8561@gmail.com>
2024-09-17 11:35:51 +0530

    add blog schema

---
+++ backend/models/blog_model.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const blogSchema = new mongoose.Schema({
+    category: {
+        type: String,
+        required: true 
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    blogImage: {
+        public_Id: {
+            type: String,
+            required: true
+        },
+        url: {
+            type: String,
+            required: true
+        }
+    },
+    discription: {
+        type: String,
+        required: true,
+        minlength: [200, "Atleast 200 characters required."]
+    },
+    adminImage: {
+        type: String,
+        required: true
+    },
+    adminName: {
+        type: String,
+        required: true
+    }
+})
+
+const Blog = mongoose.model("Blog", blogSchema);
+export default Blog;
