8f279ef07f0d2c13edc2ba53da035a5c280e361e
Yash Rajput <rajputyash8561@gmail.com>
2024-09-18 13:10:50 +0530

    Add isAuthenticated middleware for authentication using JWT

---
+++ backend/middleware/authenticateUser.js
@@ -0,0 +1,30 @@
+import userModel from "../models/user_model.js";
+import jwt from "jsonwebtoken";
+
+export const isAuthenticated = async (req, res, next) => {
+  try {
+    const jwtToken = req.cookies.jwttoken;
+    console.log("jwtToken : ", jwtToken);
+    
+    if (!jwtToken) {
+      return res
+        .status(401)
+        .json({ message: "Unauthorized access, You wants to login first." });
+    }
+    const decoded = jwt.verify(jwtToken, process.env.JWT_SECRET);
+    console.log("decode: ", decoded);
+
+    const user = await userModel.findById(decoded.userId);
+    console.log("auth User : ", user);
+    if (!user) {
+      return res.status(401).json({ message: "User not found" });
+    }
+    req.user = user;
+
+    next();
+  } catch (error) {
+    console.log(error);
+    
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+};
--- backend/routes/blog_route.js
+++ backend/routes/blog_route.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { createBlog } from "../controller/blog_controller.js"
+import { isAuthenticated } from "../middleware/authenticateUser.js";
 const router = express.Router();
 
 router.
     route("/create")
-    .post(createBlog);
+    .post(isAuthenticated, createBlog);
 
 export default router;
