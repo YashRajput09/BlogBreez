cb1ac8a88972024e873a2b9c07dc6b2ff9e88e6d
Yash Rajput <rajputyash8561@gmail.com>
2024-09-21 21:37:46 +0530

    feat(context): setup AuthContext for fetching and managing blog data globally

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,15 +9,19 @@ import Contact from './pages/Contact';
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
 import Dashboard from './pages/Dashboard';
+import { useAuth } from './context/authProvider';
 
 function App() {
   const location = useLocation();
   const hidePages = ["/login", "/signup", "/dashboard"].includes(
     location.pathname
-  )
+  );
+  const { blogs } = useAuth();
+  console.log(blogs);
+  
   return (
     <>
-      <div className="font-bold">
+      <div className="font-bold w-full h-screen bg-slate-800 text-white">
        {!hidePages && <Navebar/>}
        <Routes>
         <Route path="/" element={<Home/>}/>
---
+++ frontend/src/context/AuthProvider.jsx
@@ -0,0 +1,26 @@
+import axios from "axios";
+import React, { createContext, useContext, useEffect, useState } from "react";
+export const AuthContext = createContext(); //creates a context to that manage authenticated data (blog Info) golbally.
+
+// context provider component
+export const AuthProvider = ({ children }) => {  // takes {children} as a props, (means wraped with provider will be consider its children).
+  const [blogs, setBlogs] = useState(); // manage the blogs state, initialy blogs is undefined.
+  useEffect(() => { 
+    const fetchBlogs = async () => {
+      try {
+        const { data } = await axios.get(     //fetch Blogs using axios 
+          "http://localhost:3000/blog/all-blogs"
+        );
+        setBlogs(data); // store the data in the state, using setBlogs().
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchBlogs();
+  }, []); // '[]' indicates useEffect will run only once after component mounts.
+  return (
+    <AuthContext.Provider value={{ blogs }}>{children}</AuthContext.Provider> // provides the data to the childrean via the value prop
+  );
+};
+
+export const useAuth = () => useContext(AuthContext); // use the data 
