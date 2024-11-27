322d209d40ab9183a00026a6b6f80d0c2675ad53
Yash Rajput <rajputyash8561@gmail.com>
2024-11-27 15:16:23 +0530

    change coe url

--- backend/app.js
+++ backend/app.js
@@ -28,8 +28,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
     // origin:[process.env.FRONTEND_URL],
-    // origin: "https://breezblogs.vercel.app",
-    origin: "https://breezblogs.onrender.com",
+    origin: "https://breezblogs.vercel.app",
+    // origin: "https:git //breezblogs.onrender.com",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
 }));
