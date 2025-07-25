9906f0844e052a8f24886072d05168fd4f60b3f6
Yash Rajput <rajputyash8561@gmail.com>
2025-07-25 12:45:59 +0530

    calculate and apply analytics  to kpicards component

---
+++ frontend/src/context/BlogAnalyticsProvider.jsx
@@ -0,0 +1,36 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import axios from "axios";
+// import { AuthContext, AuthProvider } from './AuthProvider';
+
+export const BlogAnalyticsContext = createContext();
+
+export const BlogAnalyticsProvider = ({ children }) => {
+  const [analytics, setAnalytics] = useState(null);
+  // const [summaryAnalytics, setSummaryAnalytics] = useState(null);
+  const [range, setRange] = useState("7d");
+  useEffect(() => {
+    const fetchAnalyticsData = async () => {
+      try {
+        const res = await axios.get(
+          `${import.meta.env.VITE_APP_BACKEND_URL}/blog/dashboard/analysis`,
+          // {range},
+          { withCredentials: true }
+        );
+        // console.log(res.data.data);
+        console.log(res.data);
+        setAnalytics(res.data);
+        // setSummaryAnalytics(res.data.totalSummary)
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchAnalyticsData();
+  }, [range]);
+  return (
+    <BlogAnalyticsContext.Provider value={{ analytics }}>
+      {children}
+    </BlogAnalyticsContext.Provider>
+  );
+};
+
+export const useBlogAnalytics = () => useContext(BlogAnalyticsContext);
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,15 +5,18 @@ import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthProvider.jsx";
 import { InteractionProvider } from "./context/InteractionProvider.jsx";
 import { UserFeedProvider } from "./context/UserFeedProvider.jsx";
+import { BlogAnalyticsProvider } from "./context/BlogAnalyticsProvider.jsx";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <AuthProvider> 
-    <InteractionProvider> 
-      <UserFeedProvider>
-          <App />
-      </UserFeedProvider>
-    </InteractionProvider>
+    <AuthProvider>
+      <InteractionProvider>
+        <UserFeedProvider>
+          <BlogAnalyticsProvider>
+            <App />
+          </BlogAnalyticsProvider>
+        </UserFeedProvider>
+      </InteractionProvider>
     </AuthProvider>
   </BrowserRouter>
 );
